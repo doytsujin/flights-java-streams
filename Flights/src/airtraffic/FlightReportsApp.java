01732ace21fc6bbf4897bcbd17bc13e8fcbca107
Tony Piazza <tonypiazza@gmail.com>
2017-11-17 08:02:44 -0600

    Added slf4j-nop in pom.xml, removed the need for ReportDescription annotation and minor refactoring.

--- Flights/src/airtraffic/FlightReportsApp.java
+++ Flights/src/airtraffic/FlightReportsApp.java
@@ -3,7 +3,10 @@ package airtraffic;
 import static java.util.stream.Collectors.averagingInt;
 import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
+import static org.apache.commons.lang3.StringUtils.repeat;
+import static org.apache.commons.lang3.StringUtils.splitByCharacterTypeCamelCase;
 
 import java.lang.reflect.Method;
 import java.nio.file.Files;
@@ -15,12 +18,9 @@ import java.util.Map;
 import java.util.function.BiConsumer;
 import java.util.stream.Stream;
 
-import org.apache.commons.lang3.StringUtils;
 import org.beryx.textio.TextIO;
 import org.beryx.textio.TextIoFactory;
 import org.beryx.textio.TextTerminal;
-
-import airtraffic.annotation.ReportDescription;
 
 /**
  * Generate various flight statistics using Java 8 streams.
@@ -28,6 +28,10 @@ import airtraffic.annotation.ReportDescription;
  * @author tony@piazzaconsulting.com
  */
 public class FlightReportsApp {
+	private static final String REPORT_METHOD_NAME_PREFIX = "report";
+	private static final int REPORT_METHOD_PARAMETER_COUNT = 2;
+	private static final Class<?> REPORT_METHOD_RETURN_TYPE = Void.TYPE;
+
 	public static void main(String[] args) throws Exception {
 		List<Method> printMethods = 
 			getPrintMethodStream().sorted((m1, m2) -> m1.getName().compareTo(m2.getName()))
@@ -51,10 +55,11 @@ public class FlightReportsApp {
 	private static int getReportOption(List<Method> printMethods, TextIO io) {
 		TextTerminal<?> terminal = io.getTextTerminal();
 		terminal.println("Program options:\n");
-		terminal.println(" 0  Exit program");
+		String format = "%2d  %s\n";
+		terminal.printf(format, 0, "Exit program");
 		int n = 0;
 		for(Method m : printMethods) {
-			terminal.printf("%2d  %s\n", ++n, m.getName().substring(5));
+			terminal.printf(format, ++n, getReportDescription(m));
 		}
 		return io.newIntInputReader()
 				 .withDefaultValue(0)
@@ -64,18 +69,20 @@ public class FlightReportsApp {
 	}
 
 	private static String getReportDescription(Method method) {
-		return method.getDeclaredAnnotation(ReportDescription.class).value();
+		String name = method.getName().substring(REPORT_METHOD_NAME_PREFIX.length());
+		String[] words = splitByCharacterTypeCamelCase(name);
+		return Arrays.stream(words).collect(joining(" "));
+					 
 	}
 
 	private static Stream<Method> getPrintMethodStream() {
 		return Arrays.stream(FlightReportsApp.class.getDeclaredMethods())
-					 .filter(m -> m.getName().startsWith("print") && 
-							 	  m.getParameterTypes().length > 0 &&
-							 	  m.getReturnType().equals(Void.TYPE));
+					 .filter(m -> m.getName().startsWith(REPORT_METHOD_NAME_PREFIX) && 
+							 	  m.getParameterTypes().length == REPORT_METHOD_PARAMETER_COUNT &&
+							 	  m.getReturnType().equals(REPORT_METHOD_RETURN_TYPE));
 	}
 
-	@ReportDescription("Total flights from a specific origin (IATA code).")
-	public void printTotalFlightsFromOrigin(Stream<Flight> source, TextIO io) {
+	public void reportTotalFlightsFromOrigin(Stream<Flight> source, TextIO io) {
 		String origin = io.newStringInputReader()
 						  .read("Origin");
 		long count = source.filter(f -> f.notCancelled() && 
@@ -84,8 +91,7 @@ public class FlightReportsApp {
 		io.getTextTerminal().printf("Total flights from %s is %,d\n", origin, count);
 	}
 
-	@ReportDescription("Total flights to a specific destination (IATA code).")
-	public void printTotalFlightsToDestination(Stream<Flight> source, TextIO io) {
+	public void reportTotalFlightsToDestination(Stream<Flight> source, TextIO io) {
 		String destination = io.newStringInputReader()
 							   .read("Destination");
 		long count = source.filter(f -> f.notCancelled() && 
@@ -94,8 +100,7 @@ public class FlightReportsApp {
 		io.getTextTerminal().printf("Total flights to %s is %,d\n", destination, count);
 	}
 
-	@ReportDescription("Total flights from a specific origin (IATA code).")
-	public void printTotalFlightsFromOriginToDestination(Stream<Flight> source, TextIO io) {
+	public void reportTotalFlightsFromOriginToDestination(Stream<Flight> source, TextIO io) {
 		String origin = io.newStringInputReader()
 						  .read("Origin");
 		String destination = io.newStringInputReader()
@@ -104,11 +109,11 @@ public class FlightReportsApp {
 				                        f.getOrigin().getIATA().equals(origin) &&
 				                        f.getDestination().getIATA().equals(destination))
 						   .count();
-		io.getTextTerminal().printf("Total flights from %s to %s is %,d\n", origin, destination, count);
+		io.getTextTerminal().printf("Total flights from %s to %s is %,d\n", 
+									origin, destination, count);
 	}
 
-	@ReportDescription("Most flights by origin (IATA code).")
-	public void printMostFlightsByOrigin(Stream<Flight> source, TextIO io) {
+	public void reportMostFlightsByOrigin(Stream<Flight> source, TextIO io) {
 		int limit = io.newIntInputReader()
 					  .withDefaultValue(10)
 					  .withMinVal(1)
@@ -123,11 +128,11 @@ public class FlightReportsApp {
 			  .stream()
 			  .sorted((e1, e2) -> e2.getValue().compareTo(e1.getValue()))
 			  .limit(limit)
-			  .forEachOrdered(e -> terminal.printf("%3s\t%d\n", e.getKey().getIATA(), e.getValue()));
+			  .forEachOrdered(e -> terminal.printf("%3s\t%d\n", 
+					  								e.getKey().getIATA(), e.getValue()));
 	}
 
-	@ReportDescription("Top destinations from a specific origin (IATA code).")
-	public void printTopDestinationsFromOrigin(Stream<Flight> source, TextIO io) {
+	public void reportTopDestinationsFromOrigin(Stream<Flight> source, TextIO io) {
 		String origin = io.newStringInputReader()
 						  .read("Origin");
 		int limit = io.newIntInputReader()
@@ -144,11 +149,11 @@ public class FlightReportsApp {
 			  .stream()
 			  .sorted((e1, e2) -> e2.getValue().compareTo(e1.getValue()))
 			  .limit(limit)
-			  .forEachOrdered(e -> terminal.printf(" %3s\t\t%,10d\n", e.getKey().getIATA(), e.getValue()));
+			  .forEachOrdered(e -> terminal.printf(" %3s\t\t%,10d\n", 
+					  								e.getKey().getIATA(), e.getValue()));
 	}
 
-	@ReportDescription("Most popular routes.")
-	public void printMostPopularRoutes(Stream<Flight> source, TextIO io) {
+	public void reportMostPopularRoutes(Stream<Flight> source, TextIO io) {
 		int limit = io.newIntInputReader()
 				  .withDefaultValue(10)
 				  .withMinVal(1)
@@ -163,11 +168,11 @@ public class FlightReportsApp {
 			  .stream()
 			  .sorted((e1, e2) -> e2.getValue().compareTo(e1.getValue()))
 			  .limit(limit)
-			  .forEachOrdered(e -> terminal.printf("%s\t%,10d\n", e.getKey(), e.getValue().intValue()));
+			  .forEachOrdered(e -> terminal.printf("%s\t%,10d\n", 
+					  								e.getKey(), e.getValue().intValue()));
 	}
 
-	@ReportDescription("Worst average departure delay.")
-	public void printWorstAverageDepartureDelay(Stream<Flight> source, TextIO io) {
+	public void reportWorstAverageDepartureDelayByOrigin(Stream<Flight> source, TextIO io) {
 		int limit = io.newIntInputReader()
 				  .withDefaultValue(10)
 				  .withMinVal(1)
@@ -182,11 +187,11 @@ public class FlightReportsApp {
 			  .stream()
 			  .sorted((e1, e2) -> e2.getValue().compareTo(e1.getValue()))
 			  .limit(limit)
-			  .forEachOrdered(e -> terminal.printf(" %3s\t\t%.0f\n", e.getKey().getIATA(), e.getValue()));
+			  .forEachOrdered(e -> terminal.printf(" %3s\t\t%.0f\n", 
+					  								e.getKey().getIATA(), e.getValue()));
 	}
 
-	@ReportDescription("Worst average arrival delay")
-	public void printWorstAverageArrivalDelay(Stream<Flight> source, TextIO io) {
+	public void reportWorstAverageArrivalDelayByDestination(Stream<Flight> source, TextIO io) {
 		int limit = io.newIntInputReader()
 				  .withDefaultValue(10)
 				  .withMinVal(1)
@@ -201,11 +206,11 @@ public class FlightReportsApp {
 			  .stream()
 			  .sorted((e1, e2) -> e2.getValue().compareTo(e1.getValue()))
 			  .limit(limit)
-			  .forEachOrdered(e -> terminal.printf(" %3s\t\t\t%.0f\n", e.getKey().getIATA(), e.getValue()));
+			  .forEachOrdered(e -> terminal.printf(" %3s\t\t\t%.0f\n", 
+					  								e.getKey().getIATA(), e.getValue()));
 	}
 
-	@ReportDescription("Most cancelled flights by origin (IATA code).")
-	public void printMostCancelledFlightsByOrigin(Stream<Flight> source, TextIO io) {
+	public void reportMostCancelledFlightsByOrigin(Stream<Flight> source, TextIO io) {
 		int limit = io.newIntInputReader()
 				  .withDefaultValue(10)
 				  .withMinVal(1)
@@ -220,11 +225,11 @@ public class FlightReportsApp {
 			  .stream()
 			  .sorted((e1, e2) -> e2.getValue().compareTo(e1.getValue()))
 			  .limit(limit)
-			  .forEachOrdered(e -> terminal.printf(" %3s\t\t%,8d\n", e.getKey().getIATA(), e.getValue()));
+			  .forEachOrdered(e -> terminal.printf(" %3s\t\t%,8d\n", 
+					  								e.getKey().getIATA(), e.getValue()));
 	}
 
-	@ReportDescription("Most cancelled flights by carrier.")
-	public void printMostCancelledFlightsByCarrier(Stream<Flight> source, TextIO io) {
+	public void reportMostCancelledFlightsByCarrier(Stream<Flight> source, TextIO io) {
 		int limit = io.newIntInputReader()
 				  .withDefaultValue(10)
 				  .withMinVal(1)
@@ -240,15 +245,15 @@ public class FlightReportsApp {
 			  .stream()
 			  .sorted((e1, e2) -> e2.getValue().compareTo(e1.getValue()))
 			  .limit(limit)
-			  .forEachOrdered(e -> terminal.printf("%-24s\t%,8d\n", left(e.getKey(), 24), e.getValue()));
+			  .forEachOrdered(e -> terminal.printf("%-24s\t%,8d\n", 
+					  								left(e.getKey(), 24), e.getValue()));
 	}
 
 	private String left(String input, int size) {
 		return input != null && input.length() > size ? input.substring(0, size) : input;
 	}
 
-	@ReportDescription("Various carrier statistics.")
-	public void printCarrierStats(Stream<Flight> source, TextIO io) {
+	public void reportVariousCarrierStatistics(Stream<Flight> source, TextIO io) {
 		BiConsumer<Map<String, CarrierStats>, Flight> accumulator = 
 			(map, flight) -> {
 				Carrier carrier = flight.getCarrier();
@@ -295,8 +300,7 @@ public class FlightReportsApp {
 			  });
 	}
 
-	@ReportDescription("Flight counts by aircraft type.")
-	public void printFlightCountsByAircraftType(Stream<Flight> source, TextIO io) {
+	public void reportFlightCountsByAircraftType(Stream<Flight> source, TextIO io) {
 		TextTerminal<?> terminal = io.getTextTerminal();
 		terminal.println("Aircraft Type\t\t\tCount");
 		terminal.println("-------------------------------------------");
@@ -309,8 +313,7 @@ public class FlightReportsApp {
 			  .forEachOrdered(e -> terminal.printf("%-25s\t%,10d\n", e.getKey(), e.getValue()));
 	}
 
-	@ReportDescription("Flight counts by engine type.")
-	public void printFlightCountsByEngineType(Stream<Flight> source, TextIO io) {
+	public void reportFlightCountsByEngineType(Stream<Flight> source, TextIO io) {
 		TextTerminal<?> terminal = io.getTextTerminal();
 		terminal.println("Engine Type\t\t\tCount");
 		terminal.println("-------------------------------------------");
@@ -323,8 +326,7 @@ public class FlightReportsApp {
 			  .forEachOrdered(e -> terminal.printf("%-25s\t%,10d\n", e.getKey(), e.getValue()));
 	}
 
-	@ReportDescription("Flight counts by manufacturer.")
-	public void printFlightCountsByManufacturer(Stream<Flight> source, TextIO io) {
+	public void reportFlightCountsByManufacturer(Stream<Flight> source, TextIO io) {
 		TextTerminal<?> terminal = io.getTextTerminal();
 		terminal.println("Manufacturer\t\t\t Count");
 		terminal.println("-------------------------------------------");
@@ -337,8 +339,7 @@ public class FlightReportsApp {
 			  .forEachOrdered(e -> terminal.printf("%-25s\t%,10d\n", e.getKey(), e.getValue()));
 	}
 
-	@ReportDescription("Flight counts by plane year.")
-	public void printFlightCountsByPlaneYear(Stream<Flight> source, TextIO io) {
+	public void reportFlightCountsByPlaneYear(Stream<Flight> source, TextIO io) {
 		TextTerminal<?> terminal = io.getTextTerminal();
 		terminal.println("Year\t  Count");
 		terminal.println("-------------------");
@@ -353,8 +354,7 @@ public class FlightReportsApp {
 						  						  e.getValue()));
 	}
 
-	@ReportDescription("Most flights by origin state.")
-	public void printMostFlightsByOriginState(Stream<Flight> source, TextIO io) {
+	public void reportMostFlightsByOriginState(Stream<Flight> source, TextIO io) {
 		int limit = io.newIntInputReader()
 					  .withDefaultValue(10)
 					  .withMinVal(1)
@@ -373,8 +373,7 @@ public class FlightReportsApp {
 			  .forEachOrdered(e -> terminal.printf("%2s\t%,10d\n", e.getKey(), e.getValue()));
 	}
 
-	@ReportDescription("Most flights by destination state.")
-	public void printMostFlightsByDestinationState(Stream<Flight> source, TextIO io) {
+	public void reportMostFlightsByDestinationState(Stream<Flight> source, TextIO io) {
 		int limit = io.newIntInputReader()
 					  .withDefaultValue(10)
 					  .withMinVal(1)
@@ -393,8 +392,7 @@ public class FlightReportsApp {
 			  .forEachOrdered(e -> terminal.printf("%2s\t%,10d\n", e.getKey(), e.getValue()));
 	}
 
-	@ReportDescription("Most flights by a specific plane.")
-	public void printMostFlightsByPlane(Stream<Flight> source, TextIO io) {
+	public void reportMostFlightsByPlane(Stream<Flight> source, TextIO io) {
 		int limit = io.newIntInputReader()
 				  .withDefaultValue(10)
 				  .withMinVal(1)
@@ -402,7 +400,7 @@ public class FlightReportsApp {
 				  .read("Limit");
 		TextTerminal<?> terminal = io.getTextTerminal();
 		terminal.println("Tail #\t  Manufacturer\t\tModel #\t\tCount\t Daily Avg");
-		terminal.println(StringUtils.repeat("-", 67));
+		terminal.println(repeat("-", 67));
 		source.filter(f -> f.notCancelled())
 			  .collect(groupingBy(Flight::getPlane, counting()))
 			  .entrySet()
@@ -420,8 +418,7 @@ public class FlightReportsApp {
 			  });
 	}
 
-	@ReportDescription("Most flights by a plane model.")
-	public void printMostFlightsByPlaneModel(Stream<Flight> source, TextIO io) {
+	public void reportMostFlightsByPlaneModel(Stream<Flight> source, TextIO io) {
 		int limit = io.newIntInputReader()
 					  .withDefaultValue(10)
 					  .withMinVal(1)
@@ -429,7 +426,7 @@ public class FlightReportsApp {
 					  .read("Limit");
 		TextTerminal<?> terminal = io.getTextTerminal();
 		terminal.println("Manufacturer\t\t\tModel #\t\t\t  Count\t\tDaily Avg");
-		terminal.println(StringUtils.repeat("-", 82));
+		terminal.println(repeat("-", 82));
 		source.filter(f -> f.notCancelled())
 			  .map(p -> p.getPlane())
 			  .collect(groupingBy(Plane::getModel, counting()))
