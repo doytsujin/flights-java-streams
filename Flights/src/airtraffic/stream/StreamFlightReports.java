845cbf195f62587ecfc7bfb53e249206c20d737e
Tony Piazza <tonypiazza@gmail.com>
2017-12-08 15:53:51 -0600

    Minor correction to report method in StreamFlightReports class

--- Flights/src/airtraffic/stream/StreamFlightReports.java
+++ Flights/src/airtraffic/stream/StreamFlightReports.java
@@ -273,7 +273,6 @@ public class StreamFlightReports implements FlightReports {
 
       context.getRepository()
              .getFlightStream(year)
-             .parallel()
              .filter(f -> f.notCancelled() && f.notDiverted())
              .sorted(comparator)
              .limit(limit)
